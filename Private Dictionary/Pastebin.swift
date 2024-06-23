2c94a89cfd0f8e6a9566e96505144cac70ed7dfd
admin <admin@admins-MacBook-Pro.local>
2024-06-23 01:48:00 -0700

    whatidid

--- Private Dictionary/Pastebin.swift
+++ Private Dictionary/Pastebin.swift
@@ -89,3 +89,47 @@ func Publish(dict:[String:String]) async throws -> String {
         return "Error"
     }
 }
+func Publish(dict:[String:String]) async throws -> String {
+
+    var pasteString = ""
+    
+    let jsonEncoder = JSONEncoder()
+    jsonEncoder.outputFormatting = .prettyPrinted
+
+    do {
+        let encodePerson = try jsonEncoder.encode(dict)
+        pasteString = String(data: encodePerson, encoding: .utf8)!
+        print(pasteString)
+    } catch {
+        print(error.localizedDescription)
+        return "Error"
+    }
+    
+    
+    let url = URL(string: "https://pastebin.com/api/api_post.php")!
+
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.httpBody = "api_dev_key=\(APIKEY)&api_paste_code=\(pasteString)&api_option=paste".data(using: .utf8)
+    var (data, response) = try await URLSession.shared.data(for: request)
+    let statusCode = (response as! HTTPURLResponse).statusCode
+    if statusCode == 200 {
+        print("SUCCESS")
+        let dataurl = "\(String(data: data,encoding: .utf8)!)"
+//           let range:CountableClosedRange = 31...38
+        var code = ""
+        var index = 0
+        for i in dataurl {
+            if(index>20){
+                code += "\(i)"
+            }
+            index += 1
+        }
+        print(code)
+        return code
+    } else {
+        print("FAILURE")
+        print(String(data: data,encoding: .utf8))
+        return "Error"
+    }
+}
